416776e83fce2aa14f0a35c403568f6861f728e7
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-11-01 12:28:33 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Stores text a and button in variables
+const textElement = document.querySelector("#text");
+const changeColorButton = document.querySelector("#changeColorButton");
+
+function changeTextColor(){
+    const colors = ["red", "yellow", "green", "orange", "black", "blue", "hotpink"]; 
+    changeColorButton.addEventListener("click", () => {
+        const randomColor = colors[Math.floor(Math.random()* colors.length)];
+        textElement.style.color = randomColor; 
+    }); 
+}
+changeTextColor();
