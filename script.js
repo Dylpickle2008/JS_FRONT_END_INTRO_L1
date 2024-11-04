e3c2a5f71ca9666f8be229adc2ad2f7b2d24180d
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-11-04 13:28:08 +0000

    init commit

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function changeTextColor(){
     }); 
 }
 changeTextColor();
+
+const pictureElement = document.getElementById("toggle-image");
+function changePicture(){
+    const images = [
+        "https://thumb.photo-ac.com/39/39e3899e8e0b909108fffb510120aa38_t.jpeg",
+        "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg",
+        "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg"
+    ];
+    
+    let currentIndex = 0; 
+    
+    pictureElement.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        pictureElement.src = images[currentIndex];
+    });
+}
+
+changePicture();
